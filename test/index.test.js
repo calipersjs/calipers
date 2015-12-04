68336570f8632f46f8200b9441c49271b96d03a6
Marcus Gartner <marcus@lob.com>
2015-12-04 12:23:41 -0800

    Plugin support added

--- test/index.test.js
+++ test/index.test.js
@@ -5,33 +5,51 @@ var expect   = require('chai').expect;
 var calipers = require('../lib/index');
 
 describe('index', function () {
-  // var pdfPath = path.resolve(__dirname + '/fixtures/pdf/123x456.1.pdf');
-  // var expectedOutput = {
-  //   type: 'pdf',
-  //   pages: [{ width: 123, height: 456 }]
-  // };
 
-  // it('should return the correct dimensions for 123x456.pdf with callbacks', function (done) {
-  //   calipers.measure(pdfPath, function (err, result) {
-  //     expect(result).to.eql(expectedOutput);
-  //     done();
-  //   });
-  // });
+  var txtPath = path.resolve(__dirname, 'fixtures/file.txt');
+  var pngPath = path.resolve(__dirname, 'fixtures/123x456.png');
+  var output = {
+    type: 'txt',
+    pages: [{ width: 0, height: 0 }]
+  };
 
-  // it('should return the correct dimensions for 123x456.pdf with promises', function () {
-  //   return calipers.measure(pdfPath)
-  //   .bind({})
-  //   .then(function (result) {
-  //     this.result = result;
-  //   })
-  //   .finally(function () {
-  //     expect(this.result).to.eql(expectedOutput);
-  //   });
-  // });
+  var fakeTruePlugin = {
+    detect: function (buffer) { 
+      return buffer.toString('ascii', 0, 12) === 'A text file.';
+    },
+    measure: function () {
+      return output;
+    }
+  };
 
-  it('should error for an unsupported file type', function () {
-    var txtPath = path.resolve(__dirname + '/fixtures/txt/file.txt');
-    return expect(calipers.measure(txtPath)).to.be.rejectedWith(Error);
+  var fakeFalsePlugin = {
+    detect: function () { 
+      return false;
+    }
+  };
+
+  it('works with callbacks', function (done) {
+    var calipers = require('../lib/index')(fakeFalsePlugin, 'png', fakeTruePlugin);
+    calipers.measure(txtPath, function (err, result) {
+      expect(result).to.eql(output);
+      done();
+    });
+  });
+
+  it('works with promises', function () {
+    var calipers = require('../lib/index')(fakeFalsePlugin, fakeTruePlugin, 'png');
+    return calipers.measure(txtPath)
+    .then(function (result) {
+      expect(result).to.eql(output);
+    });
+  });
+
+  it('works with required plugins', function () {
+    var calipers = require('../lib/index')(fakeFalsePlugin, fakeTruePlugin, 'png');
+    return calipers.measure(pngPath)
+    .then(function (result) {
+      expect(result).to.eql({ type: 'png', pages: [{ width: 123, height: 456 }] });
+    });
   });
 
 });

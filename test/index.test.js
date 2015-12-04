6c95c4dd739c9f509694ab6e842a56cc79ffd34b
Vadym Borodin <borodean@gmail.com>
2015-12-04 15:42:54 +0200

    Remove all format definitions

--- test/index.test.js
+++ test/index.test.js
@@ -5,29 +5,29 @@ var expect   = require('chai').expect;
 var calipers = require('../lib/index');
 
 describe('index', function () {
-  var pdfPath = path.resolve(__dirname + '/fixtures/pdf/123x456.1.pdf');
-  var expectedOutput = {
-    type: 'pdf',
-    pages: [{ width: 123, height: 456 }]
-  };
+  // var pdfPath = path.resolve(__dirname + '/fixtures/pdf/123x456.1.pdf');
+  // var expectedOutput = {
+  //   type: 'pdf',
+  //   pages: [{ width: 123, height: 456 }]
+  // };
 
-  it('should return the correct dimensions for 123x456.pdf with callbacks', function (done) {
-    calipers.measure(pdfPath, function (err, result) {
-      expect(result).to.eql(expectedOutput);
-      done();
-    });
-  });
+  // it('should return the correct dimensions for 123x456.pdf with callbacks', function (done) {
+  //   calipers.measure(pdfPath, function (err, result) {
+  //     expect(result).to.eql(expectedOutput);
+  //     done();
+  //   });
+  // });
 
-  it('should return the correct dimensions for 123x456.pdf with promises', function () {
-    return calipers.measure(pdfPath)
-    .bind({})
-    .then(function (result) {
-      this.result = result;
-    })
-    .finally(function () {
-      expect(this.result).to.eql(expectedOutput);
-    });
-  });
+  // it('should return the correct dimensions for 123x456.pdf with promises', function () {
+  //   return calipers.measure(pdfPath)
+  //   .bind({})
+  //   .then(function (result) {
+  //     this.result = result;
+  //   })
+  //   .finally(function () {
+  //     expect(this.result).to.eql(expectedOutput);
+  //   });
+  // });
 
   it('should error for an unsupported file type', function () {
     var txtPath = path.resolve(__dirname + '/fixtures/txt/file.txt');

4b39d20001b7e888dab1f65ce09a58cd9cdd8472
Marcus Gartner <marcus@cockroachlabs.com>
2020-11-17 17:32:26 -0800

    implement PNG measurement in typescript
    This commit begins the transition to typescript in earnest. Measurement
    of PNGs has been implemented.
    
    Note that `npm test` runs both the current test suite and the test suite
    for the typescript implementation.
    
    This commit also adds a `npm run bench:ts` command for benchmarking the
    typescript implementation. Off that bat there is a performance boost by
    not reading from the PNG twice like Calipers v2 does (once to detect the
    file type and again to determine the dimensions).
    
        name                  old time/op  new time/op  delta
        Calipers/png-100x60    108µs ± 4%    79µs ± 2%  -26.91%  (p=0.008 n=5+5)
        Calipers/png-123x456   106µs ± 1%    80µs ± 3%  -25.15%  (p=0.008 n=5+5)
        Calipers/png-128x128   105µs ± 1%    80µs ± 8%  -23.87%  (p=0.008 n=5+5)
        Calipers/png-243x42    105µs ± 2%    78µs ± 1%  -25.03%  (p=0.008 n=5+5)
        Calipers/png-256x171   104µs ± 1%    78µs ± 1%  -25.11%  (p=0.008 n=5+5)
        Calipers/png-304x85    105µs ± 1%    79µs ± 1%  -24.51%  (p=0.008 n=5+5)
        Calipers/png-320x200   106µs ± 4%    79µs ± 1%  -25.48%  (p=0.008 n=5+5)
        Calipers/png-70x46     105µs ± 1%    79µs ± 3%  -24.98%  (p=0.008 n=5+5)
        Calipers/png-70x70     104µs ± 1%    77µs ± 2%  -25.99%  (p=0.008 n=5+5)

--- bench/index.ts
+++ bench/index.ts
@@ -21,8 +21,9 @@ import * as b from 'benny'
 import * as fs from 'fs'
 import * as path from 'path'
 
-const calipers = require('../lib')('png')
+import * as calipers from '../src'
 
+const COUNT = 5
 const ONE_BILLION = 1000000000
 
 const fixtures = path.resolve(__dirname, '../test/fixtures')
@@ -32,23 +33,27 @@ let files = fs.readdirSync(fixtures)
 files = files.filter(function (f) { return f.endsWith('.png') })
 
 // Create a benchmark for each image.
-const benchmarks = files.map(function (f) {
+const benchmarks: any[] = []
+files.forEach(function (f) {
   const [width, height, type] = f.split(/x|\./)
   const filePath = path.resolve(fixtures, f)
-  return b.add(`${type}-${width}x${height}`, async () => {
-    await calipers.measure(filePath)
+  const bench = b.add(`${type}-${width}x${height}`, async () => {
+    await calipers.measure(filePath, calipers.Format.PNG)
   })
+  for (let i = 0; i < COUNT; i++) {
+    benchmarks.push(bench)
+  }
 })
 
 // Run the benchmark suite.
 b.suite(
-  'calipers',
+  'calipers ts',
   ...benchmarks,
   b.cycle((result) => {
     // Print the results in a format that is compatible with benchstat.
     const name = result.name
     const count = result.samples
     const nsPerOp = result.details.mean * ONE_BILLION
-    return `BenchmarkCalipers/${name}\t${count}\t${nsPerOp} ns/op`
+    console.log(`BenchmarkCalipers/${name}\t${count}\t${nsPerOp} ns/op`)
   })
 )

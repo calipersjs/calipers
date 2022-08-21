9b20dddffcc33c2fb63f880e25d0499fe9fc7f9c
David Xu <thexumaker@berkeley.edu>
2022-08-20 21:09:56 -0700

    feat(Bluebird Removal): Remove bluebird from detect.test (#87)

--- test/detect.test.js
+++ test/detect.test.js
@@ -2,41 +2,35 @@
 
 var fs      = require('fs');
 var path    = require('path');
-var Promise = require('bluebird');
-var popen   = Promise.promisify(fs.open);
+var util = require('util');
+var popen   = util.promisify(fs.open);
 var detect  = require('../lib/detect');
 
-describe('detect', function () {
+describe('detect', () => {
 
   var txtPath = path.resolve(__dirname, 'fixtures/file.txt');
 
   var fakeTruePlugin = {
-    detect: function (buffer) {
+    detect: (buffer) => {
       return buffer.toString('ascii', 0, 12) === 'A text file.';
     }
   };
 
   var fakeFalsePlugin = {
-    detect: function () {
+    detect: () => {
       return false;
     }
   };
 
-  it('should return the first plugin that returns true', function () {
-    return popen(txtPath, 'r')
-    .then(function (fd) {
-      return detect(fd, [fakeFalsePlugin, fakeTruePlugin]);
-    })
-    .then(function (plugin) {
-      expect(plugin).toBe(fakeTruePlugin);
-    });
+  it('should return the first plugin that returns true', async () => {
+    const fd = await popen(txtPath, 'r');
+    const plugin = await detect(fd, [fakeFalsePlugin, fakeTruePlugin]);
+    expect(plugin).toBe(fakeTruePlugin);
   });
 
-  it('should throw an error for an unsupported file type', function () {
-    return popen(txtPath, 'r')
-    .then(function (fd) {
-      return expect(detect(fd, [fakeFalsePlugin])).rejects.toThrow(Error);
-    });
+  it('should throw an error for an unsupported file type', async () => {
+    const fd = await popen(txtPath, 'r');
+    expect(detect(fd, [fakeFalsePlugin])).rejects.toThrow(Error);
   });
 
 });
